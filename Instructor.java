ab1f2d29a9a11ab60383d19ba749e18cd299a6d0
hartmmac22 <59925490+hartmmac22@users.noreply.github.com>
2022-02-09 08:27:59 -0500

    Create Instructor.java

---
+++ Instructor.java
@@ -0,0 +1,10 @@
+public class Instructor extends Person {
+		private double salary;
+		public Instructor (String name, int birth, double salary){
+			super(name, birth);
+			this.salary = salary;
+		}
+		public String toString() {
+		return(super.toString () + salary);
+	}
+}
