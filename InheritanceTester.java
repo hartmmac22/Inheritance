1a5e45c96bcb83ae4660342e548f67d06bebda73
hartmmac22 <59925490+hartmmac22@users.noreply.github.com>
2022-02-09 08:28:40 -0500

    Create InheritanceTester.java

---
+++ InheritanceTester.java
@@ -0,0 +1,8 @@
+public class InheritanceTester {
+	public static void main(String[] args) {
+		Scholar s1 = new Scholar ("Mac", 2004, "PoliSci");
+		Scholar s2 = new Scholar ("Lindsey", 2005, "Business");
+		Instructor m1 = new Instructor ("Eliza", 2004, 100000);
+		Instructor m2 = new Instructor ("Riley", 2004, 50000);
+	}
+}
